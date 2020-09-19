6535b2af4a6a2e7c00f4fd6c9d600d9cc0199205
unknown <dev.sam@gmail.com>
2020-09-20 00:31:56 +0530

    2nd Chapter

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,19 @@ class HomePage extends StatelessWidget {
         title: Text("Awesome Flutter"),
       ),
       body: Container(
-        child: Center(child: Text("Hi Flutter")),
+        child: Center(child: 
+        Container(
+          padding: const EdgeInsets.all(15),
+          alignment: Alignment.center,
+          //color: Colors.indigoAccent,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            //shape: BoxShape.circle,
+            color: Colors.indigoAccent),
+          width: 100,
+          height: 100,
+          child: Text("I am a Box",textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),),
+        )),
       ),
     );
   }
